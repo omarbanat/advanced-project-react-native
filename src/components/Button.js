24ade0d4d7d28ae6e8f00a6c2dfa1822a2eb7958
Omar Banat <o.j.banat@gmail.com>
2023-03-17 16:51:24 +0200

    Components

---
+++ src/components/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button as PaperButton } from 'react-native-paper';
+import { theme } from '../eeecore/theme';
+
+export default function Button({ onPress, mode, style, ...props }) {
+  return (
+    <PaperButton
+      onPress={onPress}
+      style={styles.button}
+      labelStyle={styles.text}
+      mode={mode}
+      {...props}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: '100%',
+    marginVertical: 10,
+    paddingVertical: 2,
+    paddingHorizontal: 15,
+    backgroundColor: theme.colors.primary,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    lineHeight: 26,
+  },
+});
