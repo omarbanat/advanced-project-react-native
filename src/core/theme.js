fce6bd67963cce3b2dba9201b19250df0b6a7163
Omar Banat <o.j.banat@gmail.com>
2023-03-17 16:51:54 +0200

    Theme

---
+++ src/core/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    primary: '#047cfc',
+    secondary: '#474747',
+    error: '#f13a59',
+  },
+};
